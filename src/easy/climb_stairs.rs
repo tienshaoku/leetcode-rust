22fcfb1406f5850f51e4a0470d6f8f257470666a
tienshaoku <tienshaoku@gmail.com>
2025-11-13 22:22:39 +0000

    feat: improve climb_stairs

--- src/easy/climb_stairs.rs
+++ src/easy/climb_stairs.rs
@@ -1,17 +1,10 @@
 fn climb_stairs(n: i32) -> i32 {
-    if n == 1 {
-        return 1;
-    } else if n == 2 {
-        return 2;
+    let n = n as usize;
+    let mut arr = vec![1; n + 1];
+    for i in 2..n + 1 {
+        arr[i] = arr[i - 1] + arr[i - 2];
     }
-
-    let mut arr = vec![1, 2];
-    for i in 2..(n as usize) {
-        // climb_stairs(n - 1) + 1
-        // climb_stairs(n - 2) + 2
-        arr.push(arr[i - 1] + arr[i - 2]);
-    }
-    arr[n as usize - 1]
+    arr[n]
 }
 
 #[cfg(test)]
